00521f49ab6ec994d436a950102d985437d96b6c
comerciavzalef <comercialvz.alef@gmail.com>
2026-05-01 13:27:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'ponto-v9.4-master';
+var CACHE_NAME = 'ponto-v9.5-master';
 var ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', function (e) {
